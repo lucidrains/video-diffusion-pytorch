6ed37a6965b6986a3e23116b3b4e2129c38fa9d2
Phil Wang <lucidrains@gmail.com>
2022-05-10 09:01:21 -0700

    attempt to fix long description problem

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
   name = 'video-diffusion-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.3.14',
+  version = '0.3.15',
   license='MIT',
   description = 'Video Diffusion - Pytorch',
+  long_description_content_type = 'text/markdown',
   author = 'Phil Wang',
   author_email = 'lucidrains@gmail.com',
   url = 'https://github.com/lucidrains/video-diffusion-pytorch',
